127b467fe76efebce3345b07ab621b3bdc92e813
Mr-Vicente <fm.vicente@campus.fct.unl.pt>
2021-11-21 19:39:55 +0000

    In depth date dimension costumized

--- src/multi_dimension_design/dimensions/date.py
+++ src/multi_dimension_design/dimensions/date.py
@@ -3,17 +3,37 @@
 #############################
 
 # Python modules
-from dataclasses import dataclass
+import datetime
+import holidays
 
 # Local module
 from src.multi_dimension_design.dimensions.utils import WEEKDAY
 
-@dataclass
 class Date:
     """Representing the date dimension in multimodal design"""
-    key: int
+    original_key: str
+    year: int
     day_of_the_month: int
     day_of_the_week: WEEKDAY
     is_weekend: bool
     is_holiday: bool
+    def __init__(self, original_key, date):
+        self._parse_date(date)
+    def _parse_date(self, date):
+        parsed_date = datetime.datetime.strptime(date, "%d/%m/%Y %I:%M:%S %p")
+        self.year = parsed_date.year
+        self.day_of_the_month = parsed_date.day
+        self.day_of_the_week = WEEKDAY[parsed_date.weekday()]
+        self.is_weekend = self.day_of_the_week.is_weekend()
+
+        # Select country
+        us_holidays = holidays.US()
+        self.is_holiday = f'{parsed_date.day}-{parsed_date.month}-{parsed_date.year}' in us_holidays
+
+    def __str__(self):
+        return f'{self.year},' \
+               f'{self.day_of_the_month},' \
+               f'{self.day_of_the_week},' \
+               f'{self.is_weekend},' \
+               f'{self.is_holiday}'
 
