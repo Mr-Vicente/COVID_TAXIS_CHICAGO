94cb09a4e6c7b52b822982845e49d4b7e8fa67d6
Mr-Vicente <fm.vicente@campus.fct.unl.pt>
2021-11-21 19:37:06 +0000

    added __init__ for easier imports

---
+++ src/multi_dimension_design/dimensions/__init__.py
@@ -0,0 +1,7 @@
+
+from date import Date
+from facility import Facility
+from location_grid import Location_Grid
+from trip_junk import Trip_Junk
+from location import Location
+from table import Table
