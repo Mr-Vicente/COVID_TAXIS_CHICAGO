f4eadd3b3403e77a2b53f277555e371ecf977c3a
Mr-Vicente <fm.vicente@campus.fct.unl.pt>
2021-11-21 19:39:19 +0000

    Added further costumization to enum types (facility type added)

--- src/multi_dimension_design/dimensions/utils.py
+++ src/multi_dimension_design/dimensions/utils.py
@@ -6,13 +6,19 @@
 from enum import Enum
 
 class WEEKDAY(Enum):
-    MONDAY = "monday"
-    TUESDAY = "tuesday"
-    WEDNESDAY = "wednesday"
-    THURSDAY = "thursday"
-    FRIDAY = "friday"
-    SATURDAY = "saturday"
-    SUNDAY = "sunday"
+    MONDAY = 0
+    TUESDAY = 1
+    WEDNESDAY = 2
+    THURSDAY = 3
+    FRIDAY = 4
+    SATURDAY = 5
+    SUNDAY = 6
+
+    def __init__(self):
+        self.to_string = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+
+    def __str__(self):
+        return self.to_string[self.value]
 
     def is_weekend(self):
         return self == WEEKDAY.SATURDAY or self == WEEKDAY.SUNDAY
@@ -20,3 +26,9 @@ class WEEKDAY(Enum):
 class WORLD_STATUS(Enum):
     PRE_COVID = "pre-covid"
     COVID_ERA = "covid"
+
+class FACILITY(Enum):
+    HOSPITAL = "hospital"
+    CLINIC = "clinic"
+    VAX_SITE = "vax_site"
+    TEST_SITE = "test_site"
